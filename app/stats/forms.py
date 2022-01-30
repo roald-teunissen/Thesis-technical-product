17451794c32722bc1f5dd5c2131f91f7dc4b96cb
Teunisr2 <roald.teunissen@hva.nl>
2022-01-30 13:30:58 +0100

    Updated the html classes of stat forms

--- app/stats/forms.py
+++ app/stats/forms.py
@@ -6,6 +6,6 @@ TIME_SCOPES= [
     ]
     
 class StatFilterForm(forms.Form):
-    time_scope = forms.CharField(label='Time scope', widget=forms.Select(choices=TIME_SCOPES))
+    time_scope = forms.CharField(label='Time scope', widget=forms.Select(choices=TIME_SCOPES, attrs={'class': 'px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm'}))
 
     
